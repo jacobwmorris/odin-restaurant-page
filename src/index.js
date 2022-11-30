f4ecb3e09d8cdcc6022a13a44bad00fcdc3312db
jacobwmorris <jacobwmorris0@gmail.com>
2022-11-30 14:22:59 -0500

    Add contact page

--- src/index.js
+++ src/index.js
@@ -135,6 +135,37 @@ function makeMenuTab() {
     return tab;
 }
 
+//Contact tab
+function makeOdinSection() {
+    const section = makeClassDiv("body-section");
+    section.appendChild(makeTextElement("h2", "Odin"));
+    section.appendChild(makeList(["Manager", "111-111-1111", "OdinAllFather@godmail.com"]));
+    return section;
+}
+
+function makeThorSection() {
+    const section = makeClassDiv("body-section");
+    section.appendChild(makeTextElement("h2", "Thor"));
+    section.appendChild(makeList(["Head Chef", "111-111-1112", "AwesomeThunderCrusherMan420@godmail.com"]));
+    return section;
+}
+
+function makeLokiSection() {
+    const section = makeClassDiv("body-section");
+    section.appendChild(makeTextElement("h2", "Loki"));
+    section.appendChild(makeList(["I don't actually work here lul.", "Also the food is terrible.", "( ͡° ͜ʖ ͡°)."]));
+    return section;
+}
+
+function makeContactTab() {
+    const tab = makeClassDiv("tab");
+    tab.appendChild(makeTextElement("h1", "Questions? Comments? Suggestions? Contact us here!"));
+    tab.appendChild(makeOdinSection());
+    tab.appendChild(makeThorSection());
+    tab.appendChild(makeLokiSection());
+    return tab;
+}
+
 //Footer
 function makeLink(text, href) {
     const link = document.createElement("a");
@@ -171,7 +202,8 @@ function makeInitialPage() {
     const content = document.querySelector("#content");
     content.appendChild(makeNav());
     //content.appendChild(makeHomeTab());
-    content.appendChild(makeMenuTab());
+    //content.appendChild(makeMenuTab());
+    content.appendChild(makeContactTab());
     content.appendChild(makeFooter());
 }
 
